6886f7d6bbf8ac394f6c8768c73ced67b93046f3
firstmethod <firstmethod@hotmail.com>
2018-06-09 20:40:38 -0700

    Walled City Of Darkness: more work on game textfiles.

---
+++ Adventures/WalledCityOfDarkness/Game/Engine.cs
@@ -0,0 +1,45 @@
+﻿
+// Engine.cs
+
+// Copyright (c) 2014+ by Michael R. Penner.  All rights reserved
+
+using System.Collections.Generic;
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+
+namespace WalledCityOfDarkness.Game
+{
+	[ClassMappings(typeof(IEngine))]
+	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
+	{
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+
+		public override void InitMonsters()
+		{
+			base.InitMonsters();
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateMonsterSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+	}
+}
---
+++ Adventures/WalledCityOfDarkness/Game/Helpers/GameStateHelper.cs
@@ -0,0 +1,31 @@
+﻿
+// GameStateHelper.cs
+
+// Copyright (c) 2014+ by Michael R. Penner.  All rights reserved
+
+using Eamon.Framework;
+using Eamon.Framework.Helpers.Generic;
+using Eamon.Game.Attributes;
+
+namespace WalledCityOfDarkness.Game.Helpers
+{
+	[ClassMappings(typeof(IHelper<IGameState>))]
+	public class GameStateHelper : Eamon.Game.Helpers.GameStateHelper
+	{
+		public virtual new Framework.IGameState Record
+		{
+			get
+			{
+				return (Framework.IGameState)base.Record;
+			}
+
+			set
+			{
+				if (base.Record != value)
+				{
+					base.Record = value;
+				}
+			}
+		}
+	}
+}
