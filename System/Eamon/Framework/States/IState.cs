a61bb82c3c774e6556aa961324444f378e7caba4
firstmethod <firstmethod@hotmail.com>
2018-06-16 07:07:22 -0700

    Added more refinements to StateImpl/CommandImpl overhaul.  Abstracted common interface signatures into two new interfaces:  IStateSignatures and ICommandSignatures.

--- System/Eamon/Framework/States/IState.cs
+++ System/Eamon/Framework/States/IState.cs
@@ -4,48 +4,11 @@
 // Copyright (c) 2014+ by Michael R. Penner.  All rights reserved
 
 using System;
-using Enums = Eamon.Framework.Primitive.Enums;
 
 namespace Eamon.Framework.States
 {
-	public interface IState : IDisposable
+	public interface IState : IStateSignatures, IDisposable
 	{
-		#region Properties
 
-		bool GotoCleanup { get; set; }
-
-		string Name { get; set; }
-
-		IState NextState { get; set; }
-
-		bool PreserveNextState { get; set; }
-
-		bool Discarded { get; set; }
-
-		#endregion
-
-		#region Methods
-
-		void Dispose(bool disposing);
-
-		void PrintObjBlocksTheWay(IArtifact artifact);
-
-		void PrintCantGoThatWay();
-
-		void PrintCantVerbThere(string verb);
-
-		void PrintRideOffIntoSunset();
-
-		void PrintEnemiesNearby();
-
-		void ProcessEvents(long eventType);
-
-		string GetDarkName(IGameBase target, Enums.ArticleType articleType, string nameType, bool upshift, bool groupCountOne);
-
-		bool ShouldPreTurnProcess();
-
-		void Execute();
-
-		#endregion
 	}
 }
--- System/EamonRT/Framework/States/IStateImpl.cs
+++ System/EamonRT/Framework/States/IStateImpl.cs
@@ -3,44 +3,12 @@
 
 // Copyright (c) 2014+ by Michael R. Penner.  All rights reserved
 
-using Eamon.Framework;
 using Eamon.Framework.States;
-using Enums = Eamon.Framework.Primitive.Enums;
 
 namespace EamonRT.Framework.States
 {
-	public interface IStateImpl
+	public interface IStateImpl : IStateSignatures
 	{
 		IState State { get; set; }
-
-		bool GotoCleanup { get; set; }
-
-		string Name { get; set; }
-
-		IState NextState { get; set; }
-
-		bool PreserveNextState { get; set; }
-
-		bool Discarded { get; set; }
-
-		void Dispose(bool disposing);
-
-		void PrintObjBlocksTheWay(IArtifact artifact);
-
-		void PrintCantGoThatWay();
-
-		void PrintCantVerbThere(string verb);
-
-		void PrintRideOffIntoSunset();
-
-		void PrintEnemiesNearby();
-
-		void ProcessEvents(long eventType);
-
-		string GetDarkName(IGameBase target, Enums.ArticleType articleType, string nameType, bool upshift, bool groupCountOne);
-
-		bool ShouldPreTurnProcess();
-
-		void Execute();
 	}
 }
