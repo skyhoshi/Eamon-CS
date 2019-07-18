7cb03e97c68937972e1dce68caf6b82b2261676a
firstmethod <firstmethod@hotmail.com>
2019-07-18 16:56:12 -0700

    Bug fix: in Stronghold of Kahr-Dur, can't remove the magical amulet while in the forest (or temple).

---
+++ Adventures/StrongholdOfKahrDur/Game/Commands/Manipulation/RemoveCommand.cs
@@ -0,0 +1,35 @@
+﻿
+// RemoveCommand.cs
+
+// Copyright (c) 2014+ by Michael R. Penner.  All rights reserved
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
+using static StrongholdOfKahrDur.Game.Plugin.PluginContext;
+
+namespace StrongholdOfKahrDur.Game.Commands
+{
+	[ClassMappings]
+	public class RemoveCommand : EamonRT.Game.Commands.RemoveCommand, IRemoveCommand
+	{
+		public override void PlayerExecute()
+		{
+			Debug.Assert(DobjArtifact != null);
+
+			// Remove magical amulet in forest
+
+			if (Globals.GameState.Ro >= 65 && Globals.GameState.Ro != 92 && Globals.GameState.Ro != 93 && DobjArtifact.Uid == 18)
+			{
+				Globals.Out.Print("If you remove {0}, you'll be paralysed with fear!", DobjArtifact.GetDecoratedName03(false, true, false, false, Globals.Buf));
+
+				NextState = Globals.CreateInstance<IMonsterStartState>();
+			}
+			else
+			{
+				base.PlayerExecute();
+			}
+		}
+	}
+}
