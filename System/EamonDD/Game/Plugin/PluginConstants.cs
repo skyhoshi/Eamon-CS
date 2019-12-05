d3503fb61ab59d4b0236554e04d8013c128cf79f
firstmethod <firstmethod@hotmail.com>
2019-12-04 20:52:01 -0800

    Upgraded development environment to Visual Studio 2019; full recompile of Eamon CS. (Still targeting .NET Core 2.X).

--- System/EamonDD/Game/Plugin/PluginConstants.cs
+++ System/EamonDD/Game/Plugin/PluginConstants.cs
@@ -15,7 +15,7 @@ namespace EamonDD.Game.Plugin
 
 		public PluginConstants()
 		{
-			DevenvExePath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe";
+			DevenvExePath = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe";
 
 			DdProgVersion = ProgVersion;
 		}
